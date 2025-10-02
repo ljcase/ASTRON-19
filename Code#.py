1383f77e7856d56d10136ab46d899762b71cd629
Leo Case <ljcase@ucsc.edu>
2025-10-02 11:35:10 -0700

    first commit

---
+++ Code#.py
@@ -0,0 +1,16 @@
+x=.5
+y=7
+z=9
+f=.9
+#each varibvale has a value
+#now input simple equations
+print(x+f)
+print(y-z)
+print(x*z)
+#compouter outputs the answer
+#to the equation 
+
+#what is the diffence 
+#between using variables to 
+#type code and creating a function 
+#and have DEF and If??
---
+++ Practace.py
@@ -0,0 +1,10 @@
+a = ('Leo Jay Posiden Case,\n')
+#this sets a as a verable with the vale of my name
+b = ('My favorit food is pesto, Because it makes everything delicious.\n')
+#this makes b have the value 
+c = ('Hidden Figures is a very good movie and has an exelent plot.')
+#this makes c have the value of my favorit movie
+d = a + b + c
+#The equation d = a + b + c makes d have the value of a b and c combined 
+print (d)
+
